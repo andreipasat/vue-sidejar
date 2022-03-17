2f4ca1a09558d2e388201818b5ac9c71ab5c1f98
Andrei Pasat <pasat_andrei@yahoo.com>
2022-03-17 15:04:37 +0200

    bg body

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="container m-auto mt-1">
+  <div class="container m-auto mt-1 bg-gray-2">
     <RouterView />
   </div>
 </template>
